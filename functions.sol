9809f07d5abb9386db36c9c33b4f888a54d2293a
latentghost <akshat21515@iiitd.ac.in>
2023-05-22 23:13:19 +0530

    ahhh

---
+++ functions.sol
@@ -0,0 +1,11 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Functions{
+    
+    // publc --> scope of the function
+    // view --> view the output on your console
+    function Add(uint i1, uint i2) public view returns (uint){
+        uint out = i1 + i2;
+        return out;
+    }
+}
---
+++ variables.sol
@@ -0,0 +1,7 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Variables {
+    uint wallet = 500;
+    bool spend = false;
+    string notifySpend = "You have spent money";
+}
